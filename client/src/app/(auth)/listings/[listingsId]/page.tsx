fd5ca7dc3529c2f705d01e54c8548b6e1edd727a
M-Sufiyan10 <cutexxxboy90@gmail.com>
2024-05-08 13:07:43 +0500

    littele cahnges

--- client/src/app/(auth)/listings/[listingsId]/page.tsx
+++ client/src/app/(auth)/listings/[listingsId]/page.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { Calendar } from '@/components/ui/calendar'
+const data=[
+  {id:7,
+  image:"/images/Thailand.avif",
+  price:1500,
+  title:"Asia, Thailand",
+  description:"great place to visit"
+}
+]
 
 const page = () => {
   return (
+    <>
+    <div className="house pt-4 pb-4 m-2 ">
+      <div className="details">
+        <p></p>
+        <p></p>
+      </div>
+      <div className="image-loader"></div>
+      <img src="" alt="" />
+    </div>
     <div>
       <Calendar/>
     </div>
+    </>
   )
 }
 
--- client/src/app/(auth)/page.tsx
+++ client/src/app/(auth)/page.tsx
@@ -1,8 +1,10 @@
 import Title from "@/components/Title";
+import Gallery from "@/components/gallery/Gallery";
 import React from "react";
 
 const Header = () => {
   return (
+    <>
     <main
       id="hero-section"
       className="h-full w-full flex flex-col items-center justify-center relative"
@@ -17,7 +19,10 @@ const Header = () => {
       />
 
       <Title className="text-white text-5xl fade-in-5 z-10 relative md:text-9xl" />
+      <Gallery></Gallery>  
     </main>
+    
+    </>
   );
 };
 
