6b925647f6dff397117062c16e11e3c2874b0355
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-05-13 16:16:03 +0500

    footer remove from main page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,5 @@
-import Footer from "@/components/footer/Footer";
 import Authlayout from "./(auth)/layout";
 import Header from "./(auth)/page";
-import Footer from "@/components/footer/Footer";
 import Gallery from "@/components/gallery/Gallery";
 export default function Home() {
   return (
